0b0c83188d78a3829906fbb40cdc3698d735cefd
yongman Lee <dydaks7878@gmail.com>
2023-04-25 15:12:48 +0900

    Auditing 구현

--- src/main/java/com/fastcampus/projectboard/article/entity/Article.java
+++ src/main/java/com/fastcampus/projectboard/article/entity/Article.java
@@ -1,15 +1,17 @@
 package com.fastcampus.projectboard.article.entity;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Article {
 
@@ -20,22 +22,50 @@ public class Article {
     @Column(nullable = false) @Setter
     private String title;
 
-    @Column(nullable = false) @Setter
+    @Column(nullable = false, length = 10000) @Setter
     private String content;
 
     @Column @Setter
     private String hashtag;
 
+    @CreatedDate
     @Column(nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(nullable = false)
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;
 
+    @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime modifiedAt;
 
-    @Column(nullable = false)
+    @LastModifiedBy
+    @Column(nullable = false,length = 100)
     private String modifiedBy;
 
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    //new 키워드를 쓰지 않도록
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    //중복 요소를 제거, 정렬하기 위한 로직
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article)) return false;
+        Article article = (Article) o;
+        return articleId != null && articleId.equals(article.articleId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(articleId);
+    }
 }
