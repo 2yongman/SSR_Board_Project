9105bb1dac012eff691c1f12bb24bb4c0223a16f
yongman Lee <dydaks7878@gmail.com>
2023-05-02 17:43:29 +0900

    Querydsl 환경 설정

--- src/main/java/com/fastcampus/projectboard/article/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/article/repository/ArticleRepository.java
@@ -6,6 +6,15 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 //프로젝트의 repository를 rest API로 노출을 시킬것인지의 전략
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article,Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article,Long>
+//        ,
+//        //Article 안에 있는 모든 필드에 있는 기본 검색 기능 추가
+//        QuerydslPredicateExecutor<Article>,
+//        QuerydslBinderCustomizer<QArticle>
+{
+
+    //Querydsl
+
 
 }
--- src/main/java/com/fastcampus/projectboard/comment/repository/CommentRepository.java
+++ src/main/java/com/fastcampus/projectboard/comment/repository/CommentRepository.java
@@ -5,5 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface CommentRepository extends JpaRepository<Comment,Long> {
+public interface CommentRepository extends
+        JpaRepository<Comment, Long>
+//        ,
+//        QuerydslPredicateExecutor<Comment>,
+//        QuerydslBinderCustomizer<QComment>
+{
+
+
 }
