810a6cfa922b8cf4a466afd680c02889045bdedb
yongman Lee <dydaks7878@gmail.com>
2023-05-04 16:21:10 +0900

    게시판 구현 기본 골격 작성

--- src/main/java/com/fastcampus/projectboard/article/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectboard/article/controller/ArticleController.java
@@ -1,10 +1,20 @@
 package com.fastcampus.projectboard.article.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/article")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map){
+        map.addAttribute("articles,", List.of());
+        return "articles/index";
+    }
 
 }
